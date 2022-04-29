d6c859d7f56ccd3de0fdadec024f137aaab73c0c
Aleksei Tokarev <aleksei.tokarev@assurant.com>
2022-04-29 09:31:20 -0500

    initial as2-lib setup

---
+++ src/main/java/com/example/hylaas2server/HylaAs2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hylaas2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HylaAs2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HylaAs2ServerApplication.class, args);
+	}
+
+}
