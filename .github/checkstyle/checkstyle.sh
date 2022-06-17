b5b141e2e852a64118bf94c2d76ec9d0f32d4c92
Aleksei Tokarev <aleksei.tokarev@assurant.com>
2022-06-17 09:41:08 -0500

    extract version / simplify if

--- .github/checkstyle/checkstyle.sh
+++ .github/checkstyle/checkstyle.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+VERSION=10.3
 mkdir -p build/reports
 
-FILE=.github/checkstyle/checkstyle-10.3-all.jar
-if ! [[ -f "$FILE" ]]; then
+JAR_FILE=.github/checkstyle/checkstyle-${VERSION}-all.jar
+if [ ! -f "$JAR_FILE" ]; then
   echo "Downloading checkstyle jar"
-  curl --output .github/checkstyle/checkstyle-10.3-all.jar -LO https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.3/checkstyle-10.3-all.jar
+  curl --output ${JAR_FILE} -LO https://github.com/checkstyle/checkstyle/releases/download/checkstyle-${VERSION}/checkstyle-${VERSION}-all.jar
 fi
 
 java -Dconfig_loc=.github/checkstyle -Dbasedir=$(pwd) \
-  -jar .github/checkstyle/checkstyle-10.3-all.jar \
+  -jar ${JAR_FILE} \
   -c .github/checkstyle/checkstyle.xml \
   -f sarif \
   -o build/reports/checkstyle-analysis.sarif \
