965d03c2200c9bba6dc38565edae9d43871852c6
Aleksei Tokarev <aleksei.tokarev@assurant.com>
2022-06-16 23:40:54 -0500

    download checkstyle jar

--- .github/checkstyle/checkstyle.sh
+++ .github/checkstyle/checkstyle.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 mkdir -p build/reports
+
+FILE=.github/checkstyle/checkstyle-10.3-all.jar
+if ! [[ -f "$FILE" ]]; then
+  echo "Downloading checkstyle jar"
+  curl --output .github/checkstyle/checkstyle-10.3-all.jar -LO https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.3/checkstyle-10.3-all.jar
+fi
+
 java -Dconfig_loc=.github/checkstyle -Dbasedir=$(pwd) \
   -jar .github/checkstyle/checkstyle-10.3-all.jar \
   -c .github/checkstyle/checkstyle.xml \
